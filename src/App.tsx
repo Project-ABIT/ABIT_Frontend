9dcbdebdfdd1456e2272457c085070e6a41f1eca
BitByte08 <eight2823@naver.com>
2024-10-01 00:04:45 +0900

    setting project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
+import {BrowserRouter} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
-
+import Header from "./components/Header";
+import MenuBar from "./components/MenuBar";
+import HomePage from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <MenuBar />
+        <HomePage />
+      </BrowserRouter>
     </div>
   );
 }
