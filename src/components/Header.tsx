fbcf152ed8a24bfb2f26563203731435e579dfa3
BitByte08 <eight2823@naver.com>
2024-10-01 11:55:08 +0900

    Link logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import CartSymbol from'../assets/cart.svg';
 import SearchSymbol from '../assets/search.svg';
 import '../style/Header.css';
 const Header:React.FC = () =>(
   <header className="header">
     <div className="logo-container">
-      <p className="logo-text">ABIT</p>
+      <p className="logo-text"><Link to ='/'>ABIT</Link></p>
     </div>
     <div className="symbol-container">
       <div className='symbol'><img src={SearchSymbol} alt="search" className='symbol' id="search"/></div>
