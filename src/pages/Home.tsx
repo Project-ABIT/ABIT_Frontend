97541f9c25f4bcf4d8a9ab91a12e7493c32f448d
BitByte08 <eight2823@naver.com>
2024-10-01 11:55:36 +0900

    edit homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import ItemList from '../components/ItemList'
+import img from '../assets/vector.png'
 const Content = [{
   'title':'today',
-  'data': ['1','2','3','4','5']
+  'data': [{
+    'img':`${img}`,
+    'shop':'a',
+    'price':1234
+  },{
+    'img':`${img}`,
+    'shop':'b',
+    'price':1234
+  },{
+    'img':`${img}`,
+    'shop':'c',
+    'price':1234
+  },{
+    'img':`${img}`,
+    'shop':'d',
+    'price':1234
+  },{
+    'img':`${img}`,
+    'shop':'e',
+    'price':1234
+  }]
 },{
   'title':'next',
   'data':['4','5','6']
